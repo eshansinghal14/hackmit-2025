c12ce3b4e4f743f18bcb79a6f42fe773333860ad
eshansinghal14 <eshansinghal05@gmail.com>
2025-09-13 12:52:04 -0400

    removing whiteboard js and completed ai writing spline extraction

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Whiteboard from './Whiteboard';
-import './styles.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Whiteboard />);
